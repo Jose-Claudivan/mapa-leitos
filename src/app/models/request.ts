fbb86f6e3e9c481f31643ed7be59071436f4023b
juniorpires <juniorpiresupe@gmail.com>
2020-05-24 14:48:00 -0300

    configurado servidor externo

--- src/app/models/request.ts
+++ src/app/models/request.ts
@@ -1,2 +1,4 @@
-export const REQUEST_BASE_URL = 'http://localhost:3000'
+//export const REQUEST_BASE_URL = 'http://localhost:3000'
+export const REQUEST_BASE_URL = 'https://my-json-server.typicode.com/juniorpires/hackingrio-data'
+
 
