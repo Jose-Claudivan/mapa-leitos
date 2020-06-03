a56f648f2dab78449909fd661cfbadf7ed2bdec7
juniorpires <juniorpiresupe@gmail.com>
2020-06-03 15:51:44 -0300

    trocada a url de requisição

--- src/app/models/request.ts
+++ src/app/models/request.ts
@@ -1,2 +1,4 @@
-export const REQUEST_BASE_URL = 'http://localhost:8080'
+//export const REQUEST_BASE_URL = 'http://localhost:8080'
+export const REQUEST_BASE_URL = 'https://clickleito.herokuapp.com'
+
 
