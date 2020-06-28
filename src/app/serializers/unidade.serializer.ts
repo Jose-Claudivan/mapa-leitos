8ef01f8776f20be6514ffaca3bcb3ad68ed258d9
juniorpires <juniorpiresupe@gmail.com>
2020-06-28 15:41:35 -0300

    corrigido bug do cnpj

--- src/app/serializers/unidade.serializer.ts
+++ src/app/serializers/unidade.serializer.ts
@@ -4,24 +4,26 @@ import { Leito } from '../models/leito';
 
 export class UnidadeSerializer {
     fromJson(json: any): Unidade {
-      let unidade = new Unidade();
-      unidade.leito = new Leito();
+      let unidade = new Unidade()
+      unidade.leito = new Leito()
 
-      unidade.id = json.id;
-      unidade.nome = json.nome;
-      unidade.latitude = json.latitude;
-      unidade.longitude = json.longitude;
-      unidade.leito.total = json.leito.total;
-      unidade.leito.disponiveis = json.leito.disponiveis;
-      unidade.leito.id = json.leito.id;
+      unidade.id = json.id
+      unidade.cnpj = json.cnpj
+      unidade.nome = json.nome
+      unidade.latitude = json.latitude
+      unidade.longitude = json.longitude
+      unidade.leito.total = json.leito.total
+      unidade.leito.disponiveis = json.leito.disponiveis
+      unidade.leito.id = json.leito.id
 
-      
+
       return unidade;
     }
-  
+
     toJson(unidade: Unidade): any {
       let json = {
             "id" : unidade.id,
+            "cnpj" : unidade.cnpj,
             "nome" : unidade.nome,
             "latitude" : unidade.latitude,
             "longitude" : unidade.longitude,
